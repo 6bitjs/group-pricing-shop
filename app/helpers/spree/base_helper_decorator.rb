851c6e428d0fb811317bbab6194935acca803f66
Sonu <sonusingh.ror1989@gmail.com>
2018-04-19 18:18:42 +0530

    Quick changes

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -7,12 +7,13 @@ Spree::BaseHelper.module_eval do
       to_html
   end
 
-  def pricing_group_display_price(product_or_variant, currency_icon=true, current_user=nil)
+  def pricing_group_display_price(product_or_variant, currency_icon=true, user=nil)
     price = 0.0
-    spree_current_user = current_user if current_user.present?
+    user = user || spree_current_user
+       
     current_currency = Spree::Preference.find_by(key: "spree/app_configuration/currency").value unless current_currency
-    if spree_current_user.try(:pricing_group).present?
-      current_user_pricing_group = spree_current_user.pricing_group
+    if user.try(:pricing_group).present?
+      current_user_pricing_group = user.pricing_group
       product_price =  current_user_pricing_group ? 
                         product_or_variant.product_prices.find_by(pricing_group_id: current_user_pricing_group.id) :
                         nil
@@ -57,7 +58,7 @@ Spree::BaseHelper.module_eval do
     end
     product_price = pricing_group_display_price(@product, currency_icon=false)
 
-    "(Add: #{price - product_price})"
+    "(Add: #{(price - product_price).round(2)})"
   end
 
 
